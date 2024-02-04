75def3a1579a91cad6f9015a60c06c04c37d0ff2
Chandler Rose <109770223+RosePwns@users.noreply.github.com>
2024-02-03 22:53:46 -0500

    Create bii_detect.py

---
+++ Baby_Interdimensional_Internet/bii_detect.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import requests
+
+# Set the base URL for the Flask application
+base_url = 'http://83.136.254.199:32748'
+
+# Make a POST request to the root route '/' with our custom ingredients
+data = {'ingredient': 'custom_ingredient', 'measurements': '1000 + 10000'}
+response = requests.post(base_url + '/', data=data)
+print("POST /:", response.text)
